2f9b07743552a317cec03b77e5d1c9be632dcdc2
Disha Punjabi <dpunjabi@usc.edu>
2016-03-13 16:24:24 -0700

    Added files via upload

---
+++ TechWindow/TechWindow/ViewController.swift
@@ -0,0 +1,22 @@
+//
+//  ViewController.swift
+//  TechWindow
+//
+//  Created by Disha Punjabi on 3/2/16.
+//  Copyright © 2016 DCap. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+   
+
+
+}
+
